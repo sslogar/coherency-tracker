457aa56738c23370b3a1d31529e38f09ef49e845
sslogar <sslogar@ufl.edu>
2020-03-04 12:20:05 -0500

    code clean up

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import nltk
 stop_words = nltk.corpus.stopwords.words('english')
 
 new_sw = []
-for word in stop_words:
-    if word.find("'") > -1:
-        new_sw.append(word.replace("'", ""))
+new_sw = [word.replace("'", "") for word in stop_words if word.find("'") > -1]
 
 stop_words.extend(new_sw)
-more_sw = ['im', 'id', 'hed', 'shed', 'hes', 'shes', 'theyre']
+
+more_sw = ['im', 'id', 'hed', 'shed', 'hes', 'shes', 'theyre', 'ive']
 stop_words.extend(more_sw)
 
-trump = pd.read_csv('trump.csv', encoding = "ISO-8859-1")
+def process(text, stop_words):
+    sentences = processText.tokenize_sentences(text)
+    term_vec = processText.tokenize_words(sentences)
+    term_vec = processText.remove_stop_words(term_vec, stop_words)
+    norm = processText.rebuild(term_vec)
+    norm = processText.remove_empty_terms(norm)
+    return norm
 
-sentences = processText.tokenize_sentences(trump['text'])
-term_vec = processText.tokenize_words(sentences)
-# print(term_vec)
-term_vec = processText.remove_stop_words(term_vec, stop_words)
-# print(term_vec)
-norm_trump = processText.rebuild(term_vec)
-# print(norm_trump)
+trump = pd.read_csv('trump.csv', encoding = "ISO-8859-1")
+norm_trump = process(trump['text'], stop_words)
+print(norm_trump)
--- processText.py
+++ processText.py
@@ -6,14 +6,8 @@ import string
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 def tokenize_sentences(text):
-    # sub_s = sent_tokenize(line) for line in text
-    # sentences = [replace_chars(s) for s in sub_s]
-    sentences = []
-    for line in text:
-        sub_s = sent_tokenize(line)
-        for s in sub_s:
-            s = replace_chars(s)
-            sentences.append(s)
+    sub_s = [sent_tokenize(line) for line in text][0]
+    sentences = [replace_chars(s) for s in sub_s]
     return sentences
 
 def rebuild(term_vec):
@@ -30,23 +24,22 @@ def replace_chars(s):
     return s
 
 def tokenize_words(doc):
-    #tokenize into words
     punc = re.compile('[%s]' % re.escape(string.punctuation))
-    term_vec = []
-    for d in doc:
-        d=d.lower()
-        d= punc.sub('', d)
-        term_vec.append(word_tokenize(d))
+    term_vec = [d.lower() for d in doc]
+    term_vec = [punc.sub('', term) for term in term_vec]
+    term_vec = [word_tokenize(term) for term in term_vec]
     return term_vec
 
 def remove_stop_words(term_vec, sw):
     for i in range(0, len(term_vec)):
         term_list = []
+        # term_list = [term for term in term_vec[i] if term not in sw]
         for term in term_vec[i]:
             if term not in sw:
                 term_list.append(term)
         term_vec[i] = term_list
     return(term_vec)
 
-# model = Word2Vec(norm_trump, size=100, window=5, min_count=1, workers=4)
-# print(model)
+def remove_empty_terms(doc):
+    v = [sent for sent in doc if len(sent) > 0]
+    return v
