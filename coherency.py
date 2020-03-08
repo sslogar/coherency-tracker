7a0d2b931fb6a5b6a9d2d13d5273d158bfe501ac
sslogar <sslogar@ufl.edu>
2020-03-07 19:40:35 -0500

    implemented word2vec from gensim

--- coherency.py
+++ coherency.py
@@ -1,4 +1,15 @@
 from gensim.models import Word2Vec
+from nltk.tokenize import WordPunctTokenizer
 
-model = Word2Vec(text, size=100, window=5, min_count=1, workers=4)
-print(model)
+def tokenizeCorpus(text):
+    return [WordPunctTokenizer().tokenize(doc) for doc in text]
+
+def w2v(text, size, window, count, sample):
+    model = Word2Vec(text, size=size, window=window,
+            min_count=count, workers=4, sample=sample, iter=50)
+    return model
+
+def returnSimilarWords(model, searchTerms):
+    similar = {search_term: [item[0] for item in model.wv.most_similar([search_term], topn=5)]
+                for search_term in searchTerms}
+    return similar
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import processText
 import nltk
+import coherency
 
 stop_words = nltk.corpus.stopwords.words('english')
 
@@ -8,17 +9,18 @@ new_sw = [word.replace("'", "") for word in stop_words if word.find("'") > -1]
 
 stop_words.extend(new_sw)
 
-more_sw = ['im', 'id', 'hed', 'shed', 'hes', 'shes', 'theyre', 'ive']
+more_sw = ['im', 'id', 'hed', 'shed', 'hes', 'shes', 'theyre', 'ive', 'one', 'like']
 stop_words.extend(more_sw)
-print(stop_words)
+# print(stop_words)
 
 # def extend_sw(sw, arr=[]):
-#     new_sw = new_sw = [word.replace("'", "") for word in sw if word.find("'") > -1]
-#     sw.extend(new_sw)
-#     if len(arr) == 0:
+#     if len(arr) > 0:
 #         sw.extend(arr)
+#     else:
+#         new_sw = new_sw = [word.replace("'", "") for word in sw if word.find("'") > -1]
+#         sw.extend(new_sw)
 #     return sw
-#
+
 # stop_words = extend_sw(stop_words)
 
 def process(text, stop_words):
@@ -31,4 +33,8 @@ def process(text, stop_words):
 
 trump = pd.read_csv('trump.csv', encoding = "ISO-8859-1")
 norm_trump = process(trump['text'], stop_words)
+
+tokenized = coherency.tokenizeCorpus(norm_trump)
+m1 = coherency.w2v(tokenized, size=10, window=5, count=1, sample=1e-3)
+print(coherency.returnSimilarWords(m1, ['red', 'ukraine', 'omar', 'democrats', 'great', 'america', 'congress']))
 # print(norm_trump)
